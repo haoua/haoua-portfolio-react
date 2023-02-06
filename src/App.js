4d0b32ce66f69b43231469d5ef3d55dc1357295b
haoua <haouasou@gmail.com>
2023-02-06 13:26:48 +0000

    :tada: ajout structure basique header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 
+import Header from "./parts/Header";
+
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
---
+++ src/parts/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>_a_propos _projets _contact</nav>
+    </header>
+  );
+}
