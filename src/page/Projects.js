42e3dcf4c0ae9a9f654f9c301ecf4063446f17b0
haoua <haouasou@gmail.com>
2023-02-08 12:56:59 +0000

    :art: working on structure

--- src/page/Projects.js
+++ src/page/Projects.js
@@ -1,3 +1,14 @@
+import projectsData from "../data/projets.json";
+
 export default function Projects() {
-  return <div>Projets</div>;
+  return (
+    <div>
+      <h1>Projets</h1>
+      <ul>
+        <li>PHP</li>
+        <li>CSS</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
 }
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
+      <Link to="/">Haoua Soualmia</Link>
       <nav>
         _a_propos <Link to="/projects">_projets</Link> _contact
       </nav>
